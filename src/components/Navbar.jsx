601e460396f19278b64c97f17804f70f6f4fac32
Boaz <boaserem022@gmail.com>
2022-07-30 09:23:27 +0300

    nav element add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
       <div className="px-2 flex items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Brand.</h1>
+
           <ul className="hidden md:flex">
             <li>Home</li>
             <li>About</li>
@@ -15,8 +16,33 @@ const Navbar = () => {
             <li>Pricing</li>
           </ul>
         </div>
+
+        <div className="hidden pr-4 md:flex">
+          <button className="border-none bg-transparent text-black mr-4">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sing Up</button>
+        </div>
+
+        <div className="md:hidden ">
+          <FiMenu className="w-5  " />
+        </div>
       </div>
-      <FiMenu className="w-5" />
+
+      <ul className=" md:hidden text-center absolute bg-zinc-200 w-full px-8">
+        <li className="border-b-2 border-zinc-300 w-full">Home</li>
+        <li className="border-b-2 border-zinc-300 w-full">About</li>
+        <li className="border-b-2 border-zinc-300 w-full">Support</li>
+        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
+        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+
+        <div className=" flex flex-col my-4">
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
+            Sign In
+          </button>
+          <button className="px-8 py-4">Sign Up</button>
+        </div>
+      </ul>
     </div>
   );
 };
