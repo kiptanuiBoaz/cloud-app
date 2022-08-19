531184adb06e0fa6b470189ff4388435e4a31645
Boaz <boaserem022@gmail.com>
2022-08-19 13:04:41 +0300

    all-in-one done

--- src/components/AllinOne.jsx
+++ src/components/AllinOne.jsx
@@ -1,8 +1,105 @@
 import React from 'react'
+import {AiOutlineCheck} from 'react-icons/ai';
 
 function AllinOne() {
   return (
-    <div>AllinOne</div>
+    <div  className= "w-full my-32">
+        <div className=" max-w-[1280px] mx-auto px-2">
+            <h2 className=" text-5xl font-bold text-center">All-In-One Platform</h2>
+            <p className ="text-2xl py-8 text-gray-500 text-center"> lorem ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit</p>
+
+            <div className= "grid sm:grid-cols-3 lg:grid-cols-4 gap-4 px-4">
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <AiOutlineCheck className="w-7 mr-4 txt-green-600"/>
+                    </div>
+                    
+                    <div>
+                        <h3 className="text-lg  font-bold">Notification</h3>
+                        <p className=" text-lg pt-2 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id quam sit amet nunc aliquam congue</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
