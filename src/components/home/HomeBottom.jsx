10109a5716e2f50dce91c215233b0155c236927e
Boaz <boaserem022@gmail.com>
2022-08-20 17:28:56 +0300

    icons refactor

--- src/components/home/HomeBottom.jsx
+++ src/components/home/HomeBottom.jsx
@@ -4,45 +4,45 @@ import { HiDatabase } from "react-icons/hi";
 import { FaPaperPlane } from "react-icons/fa";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import Service from './Service';
-import services from "./services"
 
 const  HomeBottom = () => {
 
   return(
-            <div className="absolute flex-col flex py-8 md:min-w-[530px] 
+            <div className="absolute flex-col flex py-8 md:min-w-[650px] 
               bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2
              bg-zinc-200 border-slate-300 rounded-xl text-center shadow-xl"
             >
             
                 <p className="font-bold">Data Services</p>
                 <div className="flex px-4 py-2 flex-wrap">
-                <p className=" flex px-4 py-2   text-slate-500">
-                    <AiFillDatabase className="h-6 mr-2 text-indigo-600" /> App
-                    Security
-                </p>
-                <p className=" flex px-4 py-2  text-slate-500">
-                    <HiDatabase className="h-6 mr-2 text-indigo-600" /> Security
-                </p>
+                
 
-                <p className=" flex px-4 py-2  text-slate-500">
-                    <FaCloudUploadAlt className="h-6 mr-2 text-indigo-600" /> Cloud
-                    Data
-                </p>
-                <p className="flex px-4 py-2  text-slate-500">
-                    <FaPaperPlane className="h-6 mr-2 text-indigo-600" /> API
-                </p>
+                <Service
+                    icon={<AiFillDatabase className="h-6 mr-2 text-indigo-600"/>}
+                    name={" Security"}
+                />
+                
+                <Service
+                    icon={<HiDatabase className="h-6 mr-2 text-indigo-600"/>}
+                    name={"Security"}
+                />
+                
+                <Service
+                    icon={<FaCloudUploadAlt className="h-6 mr-2 text-indigo-600"/>}
+                    name={"Cloud Data"}
+                />
 
-                {services.map(
-                    (service) =>{ 
-                        const Icon = service.icons
-                        return (
-                        <Service
-                            icon={<Icon/>}
-                            name={services.text}
-                        />
-                    )}
-                    )
-                }
+                <Service
+                    icon={<FaPaperPlane className="h-6 mr-2 text-indigo-600"/>}
+                    name={" API"}
+                />
+
+                
+                <Service
+                    icon={<HiDatabase className="h-6 mr-2 text-indigo-600"/>}
+                    name={"Database"}
+                />
+                  
             </div>
         </div>
     )
