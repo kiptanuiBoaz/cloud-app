7c75505f8a74ec8529031312cae89c511578efa1
Boaz <boaserem022@gmail.com>
2022-08-20 11:22:21 +0300

    nav refactor 04

--- src/components/navbar/Brand.jsx
+++ src/components/navbar/Brand.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 function Brand() {
+
   return (
-    <h1 className="text-3xl font-bold mr-4 sm:text-4xl">AW<span className="text-indigo-600">3</span>SOM<span className="text-indigo-600">3</span></h1>
+    <h1 className="text-3xl font-bold mr-4 sm:text-4xl">AW<span className="text-indigo-600">3</span><span className="display-none md:display-block">SOM<span className="text-indigo-600">3</span></span></h1>
   )
 }
 
