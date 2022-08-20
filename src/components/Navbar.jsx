2653645040c357d92ed878f10631287218347a57
Boaz <boaserem022@gmail.com>
2022-08-20 05:00:58 +0300

    refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { Link,animateScroll as scroll } from 'react-scroll'
+import { Link } from 'react-scroll'
 
 import { FiMenu } from "react-icons/fi";
 import { TiTimes } from "react-icons/ti";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-
+  const active = {color:"red"};
   const handleClick = () => {
     setNav(!nav);
   };
@@ -14,14 +14,14 @@ const Navbar = () => {
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex items-center w-full h-full">
         <div className="flex w-full   items-center">
-          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Brand.</h1>
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">AW<span className="text-indigo-600">3</span>SOM<span className="text-indigo-600">3</span></h1>
          
           <ul className="hidden text-1.5xl  md:flex">
-            <li><Link  to="home"  smooth={true} duration={500}>Home</Link></li>
-            <li><Link  to="about"  smooth={true} offset={-200} duration={500} >About</Link></li>
-            <li><Link  to="support"  smooth={true} offset={-90} duration={500} >Support</Link></li>
-            <li><Link  to="platforms"  smooth={true} offset={-200} duration={500} >Platforms</Link></li>
-            <li><Link  to="pricing"  smooth={true} offset={-50} duration={500} >Pricing</Link></li>
+            <li><Link  to="home" spy={true} activeClass={active} smooth={true} duration={1000}>Home</Link></li>
+            <li><Link  to="about" spy={true} activeClass="text-indigo-600" smooth={true} offset={-200} duration={1000} >About</Link></li>
+            <li><Link  to="support" spy={true} activeClass="text-indigo-600" smooth={true} offset={-90} duration={1000} >Support</Link></li>
+            <li><Link  to="platforms" spy={true} activeClass="text-indigo-600"  smooth={true} offset={-200} duration={1000} >Platforms</Link></li>
+            <li><Link  to="pricing" spy={true} activeClass="text-indigo-600" smooth={true} offset={-50} duration={1000} >Pricing</Link></li>
             
           </ul>
         </div>
@@ -49,18 +49,18 @@ const Navbar = () => {
             : " md:hidden text-center absolute bg-zinc-200 w-full px-8"
         }
       >
-        <li className="border-b-2 border-zinc-300 w-full">Home</li>
-        <li className="border-b-2 border-zinc-300 w-full">About</li>
-        <li className="border-b-2 border-zinc-300 w-full">Support</li>
-        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
-        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link  to="home"  smooth={true} offset={-600} duration={500}>Home</Link></li>
+        <li  className="border-b-2 border-zinc-300 w-full" ><Link  to="about"  smooth={true} offset={-200} duration={500} >About</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link  to="support"  smooth={true} offset={-90} duration={500} >Support</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link  to="platforms"  smooth={true} offset={-200} duration={500} >Platforms</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link  to="pricing"  smooth={true} offset={-50} duration={500} >Pricing</Link></li>
+        
 
         <div className=" flex flex-col my-4">
-          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
-            Sign In
-          </button>
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">Sign In</button>
           <button className="px-8 py-4">Sign Up</button>
         </div>
+        
       </ul>
     </div>
   );
