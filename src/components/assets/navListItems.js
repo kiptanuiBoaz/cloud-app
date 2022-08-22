4247bc0a6402bf0f32efbbdf0f7b677f8ababe35
Boaz <boaserem022@gmail.com>
2022-08-22 19:20:29 +0300

    pricing refactor 03

--- src/components/assets/navListItems.js
+++ src/components/assets/navListItems.js
@@ -1,24 +1,24 @@
 const navlistItems = [
     {
-      key:"0",
+      key:1,
       to:"about",
       offset:-200,
       text:"About"
     },
     {
-      key:"2",
+      key:2,
       to:"support",
       offset:-90,
       text:"Support"
     },
     {
-      key:"3",
+      key:3,
       to:"platforms",
       offset:-200,
       text:"Platforms"
     },
     {
-      key:"4",
+      key:4,
       to:"pricing",
       offset:-50,
       text:"Pricing"
--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -5,6 +5,11 @@ import PricingBg from "./PricingBg";
 import PricingTop from "./PricingTop";
 
 function Pricing() {
+
+    const  premiumArr = ["Donec et diam lectus","Mauris cursus mauris ac sem"," sit amet elementum","Nulla facilisi raesent fringilla"," Lorem ipsum dolor sit amet"];
+    // const standardArr =["Etiam a ultricies sapien","Nulla facilisi raesent fringilla","Donec et diam lectus","Mauris cursus mauris ac sem","Nunc rutrum  in facilisis"];
+    // const freeArr =["Nunc rutrum  in facilisis ","Etiam a ultricies sapien","Fusce tincidunt nisl mauris","Mauris interdum quis risus ut","Nulla facilisi raesent fringilla"];
+
   return (
         <div name="pricing" className="w-full text-white my-24">
     {/* the background to the section */}
@@ -13,20 +18,17 @@ function Pricing() {
                     <PricingTop/>
 
                     <div className="grid md:grid-cols-3">
-                        {()=>{
-                            const  premiumArr = ["Donec et diam lectus","Mauris cursus mauris ac sem"," sit amet elementum","Nulla facilisi raesent fringilla"," Lorem ipsum dolor sit amet"];
-                            const standardArr =["Etiam a ultricies sapien","Nulla facilisi raesent fringilla","Donec et diam lectus","Mauris cursus mauris ac sem","Nunc rutrum  in facilisis"];
-                            const freeArr =["Nunc rutrum  in facilisis ","Etiam a ultricies sapien","Fusce tincidunt nisl mauris","Mauris interdum quis risus ut","Nulla facilisi raesent fringilla"];
-
-                            return(
-                                <Program
-                                    spanBgColor="bg-red-200"
-                                    
-                                />
-                            )
-
-                            
-                        }}
+                
+                        <Program
+                            tagBgColor="bg-red-200"
+                            titleStyle="text-red-900 bg-red-200"
+                            textColor="text-red-600"
+                            speArr = {premiumArr}
+                            btnStyle ="hover:text-red-900 bg-red-900 border-red-900"
+                            tagText = "Free"
+                            cost = "0"
+                        />
+                           
                         
                         <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
                             <span className="uppercase px-3 py-1 bg-red-200 text-red-900 rounded-2xl text-sm">Free</span>
@@ -35,13 +37,13 @@ function Pricing() {
                                 <p className="text-6xl font-bold py-4 flex"> $0<span className="text-xl text-slate-500 flex flex-col justify-end">/mo</span></p>
                             </div>
                             <p className="text-2xl py-8 text-slate-500 flex ">Utilizzato nel settore della tipografia e della stampa</p>
-                            <div className="    text-2xl">
+                            <div className=" text-2xl">
                                 <p className="  flex py-4"><BsCheckLg  className="w-7 mr-4 text-red-600"/>Nel settore  tipografia</p>
                                 <p className="flex py-4"><BsCheckLg  className="w-7 mr-4 text-red-600"/>Nel settore  tipografia</p>
                                 <p className="flex py-4"><BsCheckLg  className="w-7 mr-4 text-red-600"/>Nel settore  tipografia</p>
                                 <p className="flex py-4"><BsCheckLg  className="w-7 mr-4 text-red-600"/>Nel settore  tipografia</p>
                                 <p className="flex py-4"><BsCheckLg  className="w-7 mr-4 text-red-600"/>Nel settore  tipografia</p>
-                                <button className="w-[400px]  md:w-full sm:w-[500px] hover:text-red-900 bg-red-900 border-red-900 py-4 my-4">Get Started </button>
+                                <button className="w-[400px] md:w-full sm:w-[500px] hover:text-red-900 bg-red-900 border-red-900 py-4 my-4">Get Started </button>
                             </div>
                         </div>
 
